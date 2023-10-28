ece04c5ba2226b22e203a7426a9809fa5d74a43c
thy <1756127061@qq.com>
2023-10-28 17:11:28 +0800

    feat(sideBar): 抄了mui的实现

---
+++ src/app/result/page.tsx
@@ -0,0 +1,13 @@
+import SideBar from "@/components/SideBar";
+import Box from "@mui/material/Box";
+
+export default function Result() {
+    return (
+        <Box sx={{ display: 'flex' }}>
+            <SideBar />
+            <div>
+                123222
+            </div>
+        </Box>
+    )
+}
