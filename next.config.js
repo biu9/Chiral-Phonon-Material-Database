822acbb3b0f69e57ca88d89552f926b807b861cc
thy <1756127061@qq.com>
2023-11-16 15:48:18 +0800

    ci:

--- next.config.js
+++ next.config.js
@@ -9,6 +9,7 @@ const nextConfig = {
     config.externals = [...config.externals, { canvas: "canvas" }];  // required to make Konva & react-konva work
     return config;
   },
+  output: 'export'
 };
 
 module.exports = nextConfig
