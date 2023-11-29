4cceade2310b2ebab52e780bbf1ce36632d9884e
thy <1756127061@qq.com>
2023-11-29 16:57:00 +0800

    fix(build) relate issue: https://github.com/vercel/next.js/issues/50870

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,16 +1,5 @@
-import NextAuth, { NextAuthOptions } from "next-auth";
-import GithubProvider from "next-auth/providers/github";
-
-export const authOptions: NextAuthOptions = {
-  // Configure one or more authentication providers
-  providers: [
-    GithubProvider({
-      clientId: process.env.GITHUB_CLIENT_ID as string,
-      clientSecret: process.env.GITHUB_SECRET_ID as string,
-    }),
-  ],
-  secret: process.env.NEXTAUTH_SECRET as string,
-};
+import NextAuth from "next-auth";
+import { authOptions } from "@/conf/auth";
 
 const handler = NextAuth(authOptions);
 
